5f3379362c48b0f7d3cb0b848381d0b163338257
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-04-05 23:30:27 +0530

    making hero section

--- frontend/src/componets/Layout/UserLayOut.jsx
+++ frontend/src/componets/Layout/UserLayOut.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import Header from '../Common/Header'
+import React from "react";
+import Header from "../Common/Header";
+import Footer from "../Common/Footer";
+import { Outlet } from "react-router-dom";
 
 const UserLayOut = () => {
   return (
-    <div className=''>
-        <Header/>
+    <div className="">
+      <Header />
+      {/* Main components */}
+      <main>
+        <Outlet />
+      </main>
+      {/* Footer */}
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default UserLayOut
+export default UserLayOut;
