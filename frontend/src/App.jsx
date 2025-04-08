608d429ed6a5ad81a33369a8afc53198a1132e21
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-04-08 10:17:07 +0530

    installing some dependencies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import UserLayOut from "./componets/Layout/UserLayOut";
 import Home from "./pages/Home";
+import {Toaster} from "sonner"
+
+
+
 
 function App() {
   return (
     <BrowserRouter>
+    <Toaster position="top-right"/>
       <Routes>
         <Route path="/" element={<UserLayOut />}>
         <Route index element={<Home />}></Route>
