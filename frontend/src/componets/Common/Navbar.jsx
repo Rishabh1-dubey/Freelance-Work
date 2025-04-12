e78841c1dea0756b84f7620c1a9939afdcf187d2
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-04-13 01:13:35 +0530

    bug fix

--- frontend/src/componets/Common/Navbar.jsx
+++ frontend/src/componets/Common/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
         {/* middle section for all category */}
         <div className=" hidden md:flex space-x-6">
           <Link
-            to="/collection/:collection"
+            to="/collection/all"
             className="text-gray-700 hover:text-black text-sm font-medium uppercase"
           >
             Men
