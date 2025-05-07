a97c03b240323c547e0053a0249ba5ebc22210dd
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-05-07 12:28:15 +0530

    fix total Price error

--- backend/models/Cart.js
+++ backend/models/Cart.js
@@ -25,7 +25,7 @@ const cartSchema = new mongoose.Schema(
   {
     user: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "user",
+      ref: "User",
     },
     guestId: {
       type: String,
