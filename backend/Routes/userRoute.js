bbf602c40cec4d1b13ff114de3aec4b23b707146
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-06-28 16:32:42 +0530

    inital changes

--- backend/Routes/userRoute.js
+++ backend/Routes/userRoute.js
@@ -11,6 +11,7 @@ router.post("/register", async (req, res) => {
 
 
   
+  // battery ke lg gyi
   try {
     let user = await User.findOne({ email });
     if (user) return res.status(400).json({ message: "User already Exist" });
